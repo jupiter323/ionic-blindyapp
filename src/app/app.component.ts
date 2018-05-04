b135f683ab1dd8f2eedc3a58c2765172411fe0cc
JUPITER\JUPITER <jupiter.fierce.31@gmail.com>
2018-05-04 23:00:42 +0300

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,76 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, NavController, Nav, ViewController, Events } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
+import { IndexPage } from '../pages/index/index';
+import { Storage } from '@ionic/storage';
+import { HeaderColor } from '@ionic-native/header-color';
 import { HomePage } from '../pages/home/home';
+import { AndroidPermissions } from '@ionic-native/android-permissions';
+import { CallsPage } from '../pages/calls/calls';
+
+import { CallModalTrigger } from '../components';
+import { CallService } from '../services/call'
+
+import * as moment from 'moment';
+declare var cordova: any;
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage: any;
+  email: string;
+  isInCall = false
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+
+  constructor(private callservice: CallService, events: Events, callModal: CallModalTrigger, private headerColor: HeaderColor, platform: Platform, statusBar: StatusBar, public storage: Storage, splashScreen: SplashScreen) {
+
+
+
+
     platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
+
+      // statusBar.styleDefault();
+
       splashScreen.hide();
+
+
+      if (platform.is('cordova') && cordova.plugins.iosrtc) {
+        cordova.plugins.iosrtc.registerGlobals();
+        if (platform.is('android')) {
+          // header color setting
+
+          this.headerColor.tint("#b70360");
+
+        }
+      }
+
+      // alternating home or signIn/Up/Verify
+      storage.ready().then(() => {
+        storage.get('blindyVariables').then((val) => {
+
+          if (val) {
+            this.rootPage = HomePage;
+          } else {
+
+            this.rootPage = IndexPage;
+          }
+        }).catch(function (err) {
+          console.log(err);
+        })
+      }).catch(function (err) {
+        console.log(err);
+      });
     });
+
+    events.subscribe('call.status.isincall', status => {
+      console.debug('call status changed to ', status);
+      this.isInCall = status;
+    });
+
+
   }
+
+
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,28 +3,132 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+import { HeaderColor } from '@ionic-native/header-color';
+import { ScreenOrientation } from '@ionic-native/screen-orientation';
 import { MyApp } from './app.component';
+import { CallPage } from '../pages/call/call';
+import { ChatPage } from '../pages/chat/chat';
+import { ImagePage } from '../pages/image/image';
+import { IndexPage } from '../pages/index/index';
+import { ReportPage } from '../pages/report/report';
+import { ProviderPage } from '../pages/provider/provider';
 import { HomePage } from '../pages/home/home';
+import { SendPicPage } from '../pages/send-pic/send-pic';
+import { ModelPage } from '../pages/model/model';
+import { CallsPage } from '../pages/calls/calls';
+import { DatePage } from '../pages/date/date';
+import { SocketIoModule, SocketIoConfig } from 'ng-socket-io';
+import { IonicStorageModule } from '@ionic/storage';
+import { EmojiPickerModule } from '@ionic-tools/emoji-picker';
+import { FileTransfer, FileTransferObject } from '@ionic-native/file-transfer';
+import { File } from '@ionic-native/file';
+import { AndroidFullScreen } from '@ionic-native/android-full-screen';
+import { Camera } from '@ionic-native/camera';
+import { PhotoViewer } from '@ionic-native/photo-viewer';
+import { Config } from './config';
+import { ProfilePhotosPage } from '../pages/profile-photos/profile-photos';
+// for video
+import { ChatsPage, ChatPage1, LoginPage, AccountPage, ContactsPage } from '../components/videopages';
+import { CallModal, ContactModal, UserImage, CallModalTrigger, ContactAddModal, KeyboardAttach } from '../components';
+import { AttachmentService, AudioService, CallService, ChatService, ContactService, LoginService, SocketService, VideoService } from '../services';
+import { MomentModule } from 'angular2-moment';
+import { HttpModule } from "@angular/http";
+import { AndroidPermissions } from '@ionic-native/android-permissions';
+import { NativeAudio } from '@ionic-native/native-audio';
 
+const config: SocketIoConfig = { url: Config.socketurl, options: {} };
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    CallPage,
+    ChatPage,
+    ImagePage,
+    IndexPage,
+    DatePage,
+    SendPicPage,
+    CallsPage,
+    ReportPage,
+    ModelPage,
+    ProfilePhotosPage,
+    //  for video
+    ChatsPage,
+    ChatPage1,
+    LoginPage,
+    AccountPage,
+    CallModal,
+    ContactModal,
+    UserImage,
+    ContactsPage,
+    ContactAddModal,
+    KeyboardAttach
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    SocketIoModule.forRoot(config),
+    EmojiPickerModule.forRoot(),
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot({
+      name: '__mydb',
+      driverOrder: ['indexeddb', 'sqlite', 'websql']
+    }),
+    // for video
+    HttpModule,
+    MomentModule,
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    CallPage,
+    ChatPage,
+    ImagePage,
+    ModelPage,
+    DatePage,
+    SendPicPage,
+    CallsPage,
+    ReportPage,
+    IndexPage,
+    ProfilePhotosPage,
+    // for video
+    ChatsPage,
+    ChatPage1,
+    LoginPage,
+    AccountPage,
+    CallModal,
+    ContactModal,
+    UserImage,
+    ContactsPage,
+    ContactAddModal
   ],
   providers: [
     StatusBar,
+    ProviderPage,
+    HeaderColor,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    { provide: ErrorHandler, useClass: IonicErrorHandler },
+    Camera,
+    AndroidFullScreen,
+    ScreenOrientation,
+    PhotoViewer,
+    File,
+    FileTransfer,
+    FileTransferObject,
+    // for video
+    AttachmentService,
+    CallModalTrigger,
+    LoginService,
+    AudioService,
+    SocketService,
+    CallService,
+    ContactService,
+    VideoService,
+    ChatService,
+    AndroidPermissions,
+    NativeAudio,
+
+
+    
   ]
 })
-export class AppModule {}
+export class AppModule { }
